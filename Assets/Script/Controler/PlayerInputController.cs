329ca79fdcbac23e2678ef1ec2b606f9c838bcb3
[Tanha] <[julus8238@gmail.com]>
2023-11-29 15:23:49 +0900

    Animation update

--- Assets/Script/Controler/PlayerInputController.cs
+++ Assets/Script/Controler/PlayerInputController.cs
@@ -6,34 +6,41 @@ using UnityEngine.InputSystem;
 public class PlayerInputController : TopDownCharacterController
 {
     private Camera _camera;
+    private Animator _animator;
     private void Awake()
     {
         _camera = Camera.main;
+        _animator = GetComponent<Animator>();
     }
 
     public void OnMove(InputValue value)
     {
-        // Debug.Log("OnMove" + value.ToString());
         Vector2 moveInput = value.Get<Vector2>().normalized;
         CallMoveEvent(moveInput);
+
+        // 움직임 여부에 따라 애니메이션 변경하지 않음
     }
 
     public void OnLook(InputValue value)
     {
-        // Debug.Log("OnLook" + value.ToString());
         Vector2 newAim = value.Get<Vector2>();
         Vector2 worldPos = _camera.ScreenToWorldPoint(newAim);
-        newAim = (worldPos - (Vector2)transform.position).normalized;
+        Vector2 newAimDirection = (worldPos - (Vector2)transform.position).normalized;
 
-        if (newAim.magnitude >= .9f)
-        // Vector 값을 실수로 변환
+        if (newAimDirection.magnitude >= .9f)
         {
-            CallLookEvent(newAim);
+            CallLookEvent(newAimDirection);
         }
     }
 
     public void OnFire(InputValue value)
     {
         Debug.Log("OnFire" + value.ToString());
+    }
+
+    // 움직임 여부에 따라 애니메이션 변경하지 않음
+    private void LateUpdate()
+    {
+        _animator.SetBool("IsMoving", false);
     }
 }
